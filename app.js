55cc070dd0aa66e7c3553c2aeaebf79822f19b77
RAYDENFLY <raydenfly84@gmail.com>
2024-08-09 13:49:22 +0700

    change a picture

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use((req, res) => {
 });
 
 // Server listening
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
